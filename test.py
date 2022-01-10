afaef3695bcb7c45cf2c8f4ecf57b0784e663634
Lukasz Popek <01123885@pw.edu.pl>
2022-01-10 16:41:05 +0100

    tests erosion and dilation added

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
 import unittest
 import cv2 as cv
 import numpy as np
+from numpy.lib.index_tricks import nd_grid
 from cv2_test import print_img
 import data_backend_operations as db
 
 
 IMG = cv.imread("test\\test.png")
+
 
 import preprocessing as pr
 class TestPreprocessingMethods(unittest.TestCase):
@@ -21,9 +23,24 @@ class TestPreprocessingMethods(unittest.TestCase):
     def test_convert_RGB2HSV(self):
         img_hsv  = cv.cvtColor(IMG, cv.COLOR_BGR2HSV)
         img_hsv_test = pr.convert_BGR2HSV(IMG)
-        db.print_img(img_hsv_test)
-        db.print_img(img_hsv)
         self.assertAlmostEqual(img_hsv.all(), img_hsv_test.all(), places=None, delta = 1.1)
+
+    def test_dilation(self):
+        img = pr.convert_RGB2GRAY(IMG)
+        _, img = cv.threshold(img, 127, 255, cv.THRESH_BINARY)
+        img_dilated_test = pr.dilate_img(img, kern_size=5)
+        ker = np.ones((5,5), dtype=np.uint8)
+        img_dilated = cv.dilate(img, kernel=ker)
+        self.assertEqual(img_dilated.all(), img_dilated_test.all())
+
+    def test_erosion(self):
+        img = pr.convert_RGB2GRAY(IMG)
+        _, img = cv.threshold(img, 127, 255, cv.THRESH_BINARY)
+        img_eroded_test = pr.erode_img(img, kern_size=5)
+        ker = np.ones((5,5), dtype=np.uint8)
+        img_eroded = cv.dilate(img, kernel=ker)
+        self.assertEqual(img_eroded.all(), img_eroded_test.all())
+
 
 if __name__ == '__main__':
     unittest.main()
